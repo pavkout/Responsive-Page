9a2a38990d22430072ff8624f31380bb291754e5
Pavlos <pavkout@admin.teithe.gr>
2015-11-26 14:34:58 +0200

    Toaster
    Add notification

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
 /**
  * Created by Pavlos on 11/21/2015.
  */
-var app = angular.module("app",['ngMaterial', 'ngMdIcons', 'ui.router','angularMoment']);
+var app = angular.module("app",['ngMaterial', 'ngMdIcons', 'ui.router','angularMoment','toaster']);
 app.config(function ($mdThemingProvider) {
     var customBlueMap = $mdThemingProvider.extendPalette('light-blue', {
         'contrastDefaultColor': 'light',
